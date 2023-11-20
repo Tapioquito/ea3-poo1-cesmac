cffd75b17c1ca1a911cba435d27b4ec1e4a0f90d
Caio Porto <92833972+Tapioquito@users.noreply.github.com>
2023-11-20 15:46:55 -0300

    Update Program.cs

--- ea3-poo1-cesmac/Program.cs
+++ ea3-poo1-cesmac/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+namespace ea3_poo1_cesmac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            
+            Movie[] filmes = new Movie[]
+            {
+                new Movie("Filme1", "Diretor1", "Descrição1", "Ação", 120, false),
+                new Movie("Filme2", "Diretor2", "Descrição2", "Comédia", 90, true),
+               
+            };
+
+            
+            MenuController menuController = new MenuController();
+
+           
+            Movie selectedMovie = menuController.SelectMovie(filmes);
+
+           
+            double totalAmount = menuController.BuyTicket();
+
+            
+            Ticket ticket = new Ticket(selectedMovie, TicketType.Full, totalAmount, "18:00");
+
+           
+            Console.WriteLine($"Ingresso para o filme {ticket.Movie.Nome}");
+            Console.WriteLine($"Tipo de ingresso: {ticket.Type}");
+            Console.WriteLine($"Preço do ingresso: {ticket.Price}");
+            Console.WriteLine($"Horário da sessão: {ticket.HourofSession}");
+            Console.WriteLine($"Acesso à lanchonete: {ticket.SnackBarAccess()}");
+
+        }
+    }
+}
